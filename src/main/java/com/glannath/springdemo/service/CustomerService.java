ee35a4e83ffffc1b1581376cfeee73907590713a
glannath <jakub.mateja@outlook.com>
2020-07-30 08:56:28 +0200

    adding core application

---
+++ src/main/java/com/glannath/springdemo/service/CustomerService.java
@@ -0,0 +1,17 @@
+package com.glannath.springdemo.service;
+
+import java.util.List;
+
+import com.glannath.springdemo.entity.Customer;
+
+public interface CustomerService {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
